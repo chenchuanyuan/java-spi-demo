8541da1698429f987211b12a45f8866398f0d0b0
陈传源 <szuccy@163.com>
2019-10-30 10:43:48 +0800

    init

--- demo/src/main/java/com/spi/DbSearch.java
+++ demo/src/main/java/com/spi/DbSearch.java
@@ -4,6 +4,7 @@ package com.spi;
  * @author CCY
  */
 public class DbSearch implements Search {
+    @Override
     public String doWork(String key) {
         return "方法运行，使用方式DbSearch :" + key;
     }
--- demo/src/main/java/com/spi/FileSearch.java
+++ demo/src/main/java/com/spi/FileSearch.java
@@ -4,6 +4,7 @@ package com.spi;
  * @author CCY
  */
 public class FileSearch implements Search {
+    @Override
     public String doWork(String key) {
         return "方法运行，使用方式FileSearch :" + key;
     }
